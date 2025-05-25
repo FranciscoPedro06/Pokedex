57ba39a32b636c2123e71a54e29d85b5108d6756
FranciscoPedro06 <pedro2006francisco@gmail.com>
2025-05-24 22:32:07 -0300

    Atualizando o protocolo http

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,7 @@ const limit = 10;
 const url = "https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit{limit}";
 
 fetch(url)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {
-    console.log('Requisição concluida!')
-  });
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Requisição concluida!"));
