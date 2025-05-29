0fd2139c4780e4c7398544125b75eb600f2be991
FranciscoPedro06 <pedro2006francisco@gmail.com>
2025-05-28 22:09:48 -0300

    Update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,38 @@ const offset = 0;
 const limit = 10;
 const url = "https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit{limit}";
 
+function convertPokemonToLi(pokemon) {
+  return `
+      <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+              <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+              </ol>
+
+              <img
+                src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png"
+                alt="${pokemon.name}"
+              />
+            </div>
+      </li>
+    `
+}
+
 fetch(url)
   .then((response) => response.json())
-  .then((jsonBody) => console.log(jsonBody))
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemonList) => {
+    
+    for (let i = 0; i < pokemonList.length; i++) {
+      const pokemon = pokemonList[i];
+      console.log(convertPokemonToLi(pokemon))
+
+      console.log(document.getElementsByClassName("pokemons"))
+    }
+
+  })
   .catch((error) => console.log(error))
-  .finally(() => console.log("Requisição concluida!"));
